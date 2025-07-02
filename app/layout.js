cd5e9253e24ed7e38b36de0c6936e2fb17daaa61
nobgkai <okaivit@gmail.com>
2025-07-02 11:45:45 +0700

    update55

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="th" className={sarabun.variable}>
+    <html lang="en">
       <body>
         <div className="container-fluid">
           <Navarbar />
